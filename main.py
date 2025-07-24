02099c990be05d41f6bccb2e8828ac2e60cfa282
marykaten <marykaten107@gmail.com>
2025-07-24 14:38:54 -0400

    main.py
    outdated data

---
+++ main.py
@@ -0,0 +1,74 @@
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import precision_score
+
+matches = pd.read_csv("matches.csv", index_col=0)
+
+matches["date"] = pd.to_datetime(matches["date"])
+matches["venue_code"] = matches["venue"].astype("category").cat.codes
+matches["opp_code"] = matches["opponent"].astype("category").cat.codes
+matches["hour"] = matches["time"].str.replace(":.+","", regex=True).astype("int")
+matches["day_code"] = matches["date"].dt.dayofweek
+matches["target"] = (matches["result"] == "W").astype("int")
+
+rf = RandomForestClassifier(n_estimators=50, min_samples_split=10, random_state=1)
+train = matches[matches["date"] < '2022-01-01']
+test = matches[matches["date"] > '2022-02-01']
+
+predictors = ['venue_code', 'opp_code', 'hour', 'day_code']
+rf.fit(train[predictors], train["target"])
+
+predictions = rf.predict(test[predictors])
+
+accuracy = accuracy_score(test["target"], predictions)
+
+combined = pd.DataFrame(dict(actual=test["target"], predicted=predictions))
+pd.crosstab(index=combined["actual"], columns=combined["predicted"])
+
+precision_score(test["target"], predictions)
+grouped_matches = matches.groupby("team")
+
+def rolling_averages(group,cols,new_cols):
+    group = group.sort_values("date")
+    rolling_stats = group[cols].rolling(3,closed='left').mean()
+    group[new_cols] = rolling_stats
+    group = group.dropna(subset=new_cols)
+    return group
+
+cols = ["gf","ga","sh","sot","dist","fk","pk","pkatt"]
+new_cols = [f"{c}_rolling" for c in cols]
+
+matches_rolling = matches.groupby("team").apply(lambda x: rolling_averages(x,cols,new_cols))
+matches_rolling = matches_rolling.droplevel('team')
+matches_rolling.index = range(matches_rolling.shape[0])
+
+def make_predictions(data, predictors):
+    train = data[data["date"] < '2022-01-01']
+    test = data[data["date"] > '2022-02-01']
+    rf.fit(train[predictors], train["target"])
+    predictions = rf.predict(test[predictors])
+    combined = pd.DataFrame(dict(actual=test["target"], predicted=predictions), index=test.index)
+    precision = precision_score(test["target"], predictions)
+    return combined, precision
+
+combined, precision = make_predictions(matches_rolling, predictors+new_cols)
+
+combined = combined.merge(matches_rolling[['date','team','opponent','result']], left_index=True, right_index=True)
+
+class MissingDict(dict):
+    __missing__ = lambda self, key: key
+
+map_values = {
+    "Brighton and Hove Albion": "Brighton",
+    "Manchester United": "Manchester",
+    "Newcastle United": "Newcastle Utd",
+    "Tottenham Hotspur": "Tottenham",
+    "West Ham United": "West Ham",
+    "Wolverhampton Wanderers": "Wolves"
+}
+mapping = MissingDict(**map_values)
+combined["new_team"] = combined["team"].map(mapping)
+
+merged = combined.merge(combined,left_on=["date","new_team"], right_on=["date","opponent"])
+merged[(merged["predicted_x"] == 1) & (merged["predicted_y"] == 0)]["actual_x"].value_counts()
